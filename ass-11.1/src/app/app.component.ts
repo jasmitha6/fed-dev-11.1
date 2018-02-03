a040ae288b7c0dbab3a1571c730e0a1d6c5125ec
jasmitha <jasmieyjas@gmail.com>
2018-02-03 22:59:50 +0530

    Add files via upload

---
+++ ass-11.1/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ListService } from "app/service/list.service";
+import { DropDownService } from "app/service/drop-down.service";
+// defining component detail like selector ,templateUrl,styleUrls
+@Component({
+  selector: 'app-component',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ListService]
+})
+export class RootComponent {
+  title: string;
+  name: string;
+  lastName: string;
+  userDetail = {};
+  arrayList: any[] = [];
+  genderArray: any[] = [];
+  searchData: string;
+  // defining constructor with _listservice and _dropdownservice as a private property
+  constructor(private _listService: ListService, private _dropDownService: DropDownService) { }
+
+  ngOnInit() {
+    this.userDetail = {
+      name: '',
+      lastName: '',
+      age: '',
+      genderType: '',
+      date: ''
+    }
+    this.genderArray = this._dropDownService.getDropDown();
+    this.searchData = '';
+  }
+
+
+  submit(values) {
+
+    let model = {
+      name: values.name,
+      lastName: values.lastName,
+      age: values.age,
+      gender: values.gender,
+      date: values.date
+    }
+    this._listService.addList(model);
+    this.arrayList = this._listService.getList();
+  }
+}
